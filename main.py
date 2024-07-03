c874578170023efc3410535940ea94a90edfa38e
sonialiao <sonialiao131@hotmail.com>
2024-07-03 16:14:16 -0400

    added ORM so that users from form are recorded in site.db

--- main.py
+++ main.py
@@ -2,11 +2,27 @@ import git
 from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)       
 proxied = FlaskBehindProxy(app)             
 
-app.config['SECRET_KEY'] = '3af78194d11f5f973dd0c3f82a7be527'
+app.config['SECRET_KEY'] = '3af78194d11f5f973dd0c3f82a7be527'   # secret key for form submissions
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'     # setup database
 
+# User class for database
+db = SQLAlchemy(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+    
+with app.app_context():
+    db.create_all()
+   
 
 @app.route("/")   
 @app.route("/home")
@@ -20,11 +36,17 @@ def about():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    if form.validate_on_submit(): # checks if entries are valid
+    if form.validate_on_submit():   # checks if entries are valid
+
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home')) # if so - send to home page
+
+        return redirect(url_for('home'))    # if so - send to home page
     return render_template('register.html', title='Register', form=form)
 
+# auto-deployment with webhooks
 @app.route("/update_server", methods=['POST'])
 def webhook():
    if request.method == 'POST':
